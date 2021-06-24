1cd824d411694ff3fbecfa3abd0d47b202645022
fernando pimenta <fernandopimentadev@gmail.com>
2021-06-24 07:22:54 -0300

    fix(ios-plugin):ajust swift code

--- ios/Classes/SwiftMarketingcloudsfPlugin.swift
+++ ios/Classes/SwiftMarketingcloudsfPlugin.swift
@@ -13,14 +13,14 @@ public class SwiftMarketingcloudsfPlugin: NSObject, FlutterPlugin {
  
     var window: UIWindow?
     
-    /
+    
     
     let inbox = false
     let location = false
     let analytics = true
     
     @discardableResult
-    func configureMarketingCloudSDK(appID: String,accessToken: String,appEndpoint: String,mid: String,) -> Bool {
+    func configureMarketingCloudSDK(appID: String,accessToken: String,appEndpoint: String,mid: String) -> Bool {
         let builder = MarketingCloudSDKConfigBuilder()
             .sfmc_setApplicationId(appID)
             .sfmc_setAccessToken(accessToken)
@@ -65,9 +65,9 @@ public class SwiftMarketingcloudsfPlugin: NSObject, FlutterPlugin {
       switch (call.method) {
       case "inAppMenssage":  result(FlutterError.init(code: "errorSetDebug", message: "data or format error", details: nil))
       
-      case "trackCart": MarketingCloudSDK.sharedInstance().sfmc_trackCartContents(call.arguments)
+      case "trackCart": MarketingCloudSDK.sharedInstance().sfmc_trackCartContents(call.arguments as! [AnyHashable : Any])
       
-      case "trackConversion": MarketingCloudSDK.sharedInstance().sfmc_trackCartConversion(call.arguments)
+      case "trackConversion": MarketingCloudSDK.sharedInstance().sfmc_trackCartConversion(call.arguments as! [AnyHashable : Any])
       
       case "trackPageViews": if let args = call.arguments as? Dictionary<String, Any>,
           let url = args["url"] as? String,
@@ -75,7 +75,7 @@ public class SwiftMarketingcloudsfPlugin: NSObject, FlutterPlugin {
           let item = args["item"] as? String,
           let search = args["search"] as? String {
         
-           MarketingCloudSDK.sharedInstance().sfmc_trackPageView(url,title,item,search)
+        MarketingCloudSDK.sharedInstance().sfmc_trackPageView(withURL: url,title: title,item: item,search: search)
 
         } else {
           result(FlutterError.init(code: "errorSetDebug", message: "data or format error", details: nil))
@@ -90,7 +90,7 @@ public class SwiftMarketingcloudsfPlugin: NSObject, FlutterPlugin {
           let appEndpoint = args["appEndpoint"] as? String,
           let mid = args["mid"] as? String {
         
-          self.configureMarketingCloudSDK(appID,accessToken,appEndpoint,mid)
+        self.configureMarketingCloudSDK(appID: appID,accessToken: accessToken,appEndpoint: appEndpoint,mid: mid)
 
         } else {
           result(FlutterError.init(code: "errorSetDebug", message: "data or format error", details: nil))
@@ -173,129 +173,22 @@ public class SwiftMarketingcloudsfPlugin: NSObject, FlutterPlugin {
       
       case "logSdkState":  result(MarketingCloudSDK.sharedInstance().sfmc_getSDKState())
      
-      case "requestNotificationPermissions":
-          let registeredToMarketingCloud = self.configureMarketingCloudSDK()
-
-            logMessage("requestNotificationPermissions registeredToMC: \(registeredToMarketingCloud)")
-
-            if (!registeredToMarketingCloud) {
-                result(FlutterError(code: String(format: "Error %ld", 897),
-                                    message: "Failed to register with marketing cloud",
-                                    details: "MarketingCloudSDK sfmc_configure failed with error"))
-                return
-            }
-            
-            let arguments = args as? Dictionary<String, Any>
-            
-            if #available(iOS 10.0, *) {
-                logMessage("request ios > 10")
-                
-                var authOptions: UNAuthorizationOptions = []
-                let provisional = (arguments?["provisional"] as? Bool) ?? false
-                let soundSelected = (arguments?["sound"] as? Bool) ?? false
-                let alertSelected = (arguments?["alert"] as? Bool) ?? false
-                let badgeSelected = (arguments?["badge"] as? Bool) ?? false
-                
-                if (soundSelected) {
-                    authOptions.insert(.sound)
-                }
-                if (alertSelected) {
-                    authOptions.insert(.alert)
-                }
-                if (badgeSelected) {
-                    authOptions.insert(.badge)
-                }
-
-                var isAtLeastVersion12: Bool = false
-                if #available(iOS 12, *) {
-                    isAtLeastVersion12 = true
-                    
-                    if (provisional) {
-                        authOptions.insert(.provisional)
-                    }
-                } else {
-                    isAtLeastVersion12 = false
-                }
-                
-                let center = UNUserNotificationCenter.current()
-
-                center.delegate = self
-                
-                center.requestAuthorization(options: authOptions, completionHandler: { granted, error in
-                    if error != nil {
-                        self.logMessage("Something went wrong, error found")
-                        result(self.getFlutterError(error))
-                        
-                        return
-                    }
-                    
-                    if !granted {
-                        self.logMessage("Something went wrong, permission not granted")
-                        result(self.getFlutterError(error))
-                        
-                        return
-                    } else {
-                        let deviceToken = MarketingCloudSDK.sharedInstance().sfmc_deviceToken()
-
-                        if deviceToken == nil {
-                            self.logMessage("error: no token - was UIApplication.shared.registerForRemoteNotifications() called?")
-                        } else {
-                            let token = deviceToken ?? "** empty **"
-                            self.logMessage("success: token - was \(token)")
-                        }
-                    }
-
-                 
-                    UNUserNotificationCenter.current().getNotificationSettings(completionHandler: { settings in
-                        let settingsDictionary = [
-                            "sound": NSNumber(value: settings.soundSetting == .enabled),
-                            "badge": NSNumber(value: settings.badgeSetting == .enabled),
-                            "alert": NSNumber(value: settings.alertSetting == .enabled),
-                            "provisional": NSNumber(value: granted && provisional && isAtLeastVersion12)
-                        ]
-                        self.channel?.invokeMethod("onIosSettingsRegistered", arguments: settingsDictionary)
-                    })
-                    
-                    result(NSNumber(value: granted))
-                })
-                
-                UIApplication.shared.registerForRemoteNotifications()
-            } else {
-                logMessage("request ios < 10")
-                var notificationTypes = UIUserNotificationType(rawValue: 0)
-                let soundSelected = (arguments?["sound"] as? Bool) ?? false
-                let alertSelected = (arguments?["alert"] as? Bool) ?? false
-                let badgeSelected = (arguments?["badge"] as? Bool) ?? false
-                
-                if (soundSelected) {
-                    notificationTypes.insert(.sound)
-                }
-                if (alertSelected) {
-                    notificationTypes.insert(.alert)
-                }
-                if (badgeSelected) {
-                    notificationTypes.insert(.badge)
-                }
-                
-                let settings = UIUserNotificationSettings(types: notificationTypes, categories: nil)
-                UIApplication.shared.registerUserNotificationSettings(settings)
-
-                UIApplication.shared.registerForRemoteNotifications()
-                result(NSNumber(value: true))
-            }
-        } else if (call.method == "configure") {
+     
+    
+        
+      case ("configure") :
             UIApplication.shared.registerForRemoteNotifications()
 
-            if launchNotification != nil {
+            /*if launchNotification != nil {
                 self.channel?.invokeMethod("onLaunch", arguments: launchNotification!)
             }
 
-            result(nil)
-
-
+            result(nil)*/
+        result(FlutterMethodNotImplemented)
+      
       default: result(FlutterMethodNotImplemented)
 
-      }
+      
     
     
   }
@@ -303,79 +196,11 @@ public class SwiftMarketingcloudsfPlugin: NSObject, FlutterPlugin {
 
 
 
-    // MARK: - app delegate
-    public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        MarketingCloudSDK.sharedInstance().sfmc_setDeviceToken(deviceToken)
-    }
+     
+
     
-    public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        logMessage("\(error.localizedDescription)")
-    }
-
-    public func application(_ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any]) -> Bool {
-        launchNotification = launchOptions[UIApplication.LaunchOptionsKey.remoteNotification] as? [AnyHashable: Any]
-        return true
-    }
-
-    public func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings) {
-        let settingsDictionary = [
-            "sound": NSNumber(value: notificationSettings.types.rawValue & UIUserNotificationType.sound.rawValue != 0),
-            "badge": NSNumber(value: notificationSettings.types.rawValue & UIUserNotificationType.badge.rawValue != 0),
-            "alert": NSNumber(value: notificationSettings.types.rawValue & UIUserNotificationType.alert.rawValue != 0),
-            "provisional": NSNumber(value: false)
-        ]
-        self.channel?.invokeMethod("onIosSettingsRegistered", arguments: settingsDictionary)
-    }
-
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
-        MarketingCloudSDK.sharedInstance().sfmc_setNotificationUserInfo(userInfo)
-
-        if (resumingFromBackground) {
-            channel?.invokeMethod("onResume", arguments: userInfo)
-        } else {
-            channel?.invokeMethod("onMessage", arguments: userInfo)
-        }
-
-        for key in userInfo.keys {
-            guard let key = key as? String else {
-                continue
-            }
-            if let object = userInfo[key] {
-                logMessage("property value: \(object)")
-            }
-        }
-    }
-
-    public func applicationDidEnterBackground(_ application: UIApplication) {
-        resumingFromBackground = true
-    }
-
-    public func applicationDidBecomeActive(_ application: UIApplication) {
-        resumingFromBackground = false
-        application.applicationIconBadgeNumber = 1
-        application.applicationIconBadgeNumber = 0
-    }
-    
-    @available(iOS 10, *)
-    public func userNotificationCenter(_ center: UNUserNotificationCenter,
-        didReceive response: UNNotificationResponse,
-        withCompletionHandler completionHandler: @escaping () -> Void) {
-        MarketingCloudSDK.sharedInstance().sfmc_setNotificationRequest(response.notification.request)
-        completionHandler()
-    }
-
-    @available(iOS 10, *)
-    public func userNotificationCenter(_ center: UNUserNotificationCenter,
-        willPresent notification:UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler(UNNotificationPresentationOptions.alert)
-    }
-
-    private func logMessage(_ s: String) {
-        print("MC: \(s)")
-    }
 
 
 
+}
 }
